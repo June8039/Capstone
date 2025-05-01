480cc6b7048c1451aa92246d2b44bd946a4230ab
June8039 <yeonlee8039@gmail.com>
2025-05-01 19:02:17 +0900

    Feat: added a super basic screen layout for the exercise_screen, would need to fix this for better design, interactive screen

--- lib/screens/exercise_screen.dart
+++ lib/screens/exercise_screen.dart
@@ -7,8 +7,46 @@ class ExerciseScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Exercises")),
-      body: const Center(
-        child: Text("Exercise Page"),
+      body: Center(
+        child: Container(
+          width: 300,
+          padding: EdgeInsets.all(16),
+          child: const Column(
+            crossAxisAlignment:CrossAxisAlignment.start,
+            children: [
+              Text('운동계획', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(height: 16),
+              Text('운동 1'),
+              Padding(
+                  padding: EdgeInsets.only(left: 16.0),
+                child: Column(
+                  crossAxisAlignment:
+                  CrossAxisAlignment.start,
+                  children: [
+                    Text('난이도 1'),
+                    Text('난이도 2'),
+                    Text('난이도 3'),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              Text('운동 2'),
+              Padding(
+                padding: EdgeInsets.only(left: 16.0),
+                child: Column(
+                  crossAxisAlignment:
+                  CrossAxisAlignment.start,
+                  children: [
+                    Text('난이도 1'),
+                    Text('난이도 2'),
+                    Text('난이도 3'),
+                  ],
+                ),
+              )
+
+            ],
+          )
+        )
       ),
     );
   }
